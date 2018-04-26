9fb5fae310428b99379530994fd0e1393e81d42e
Karjalainen <julius.karjalainen@cs.tamk.fi>
2018-04-26 05:22:47 +0300

    Frontend crazyness vol 2

--- blog-app/index.js
+++ blog-app/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
+console.log("dwd")
 ReactDOM.render(<App src="http://localhost:8080/posts/1"/>, document.getElementById('root'));
-registerServiceWorker();
+
